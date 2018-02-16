2b146abfd34c2f63f3cf2062b8f380bd00ba77ea
Aluno Fiar Logatti <aluno@fiar.local>
2018-02-16 21:29:54 -0200

    implantação

---
+++ ProjAula2/Carro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjAula2
+{
+    class Carro
+    {
+
+        #region propriedades
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public String Cor { get; set; }
+
+        #endregion propriedades
+
+        #region metodos
+
+        public string Imprimir()
+        {
+
+
+            return "Id: " + this.Id + "\nNome: " + this.Nome +
+                "\nCor: " + this.Cor
+                ;
+        }
+
+        #endregion metodos
+
+    }
+}
